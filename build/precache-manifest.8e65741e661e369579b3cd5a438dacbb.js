cec29b51465bbea7ffcb46e136a1f9dcd22372c3
zhaochunzheng <zhaochunzheng@didiglobal.com>
2020-07-10 17:23:19 +0800

    <built>

---
+++ build/precache-manifest.8e65741e661e369579b3cd5a438dacbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "857f2ef1c6c294bcb891330bcbc51e7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac9cef9c8e4e29617f0b",
+    "url": "/static/css/main.35c5ea2b.chunk.css"
+  },
+  {
+    "revision": "4df70caa18c2f5a4179f",
+    "url": "/static/js/2.031b8a7f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.031b8a7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac9cef9c8e4e29617f0b",
+    "url": "/static/js/main.1742787e.chunk.js"
+  },
+  {
+    "revision": "b6acc35aab11631c997e",
+    "url": "/static/js/runtime-main.369ed2c2.js"
+  }
+]);
